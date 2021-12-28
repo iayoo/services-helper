0c3a0d7238d8c98dbcd36f2287469cae38713789
Iayoo <vilnius94@163.com>
2021-12-28 17:44:21 +0800

    # 新增分页配置

--- src/thinkphp/ServiceProvider.php
+++ src/thinkphp/ServiceProvider.php
@@ -7,6 +7,7 @@
 namespace Iayoo\ServiceHelper\thinkphp;
 
 use Iayoo\ServiceHelper\ServiceFactory;
+use think\facade\Request;
 use think\Model;
 
 class ServiceProvider extends ServiceFactory
@@ -14,15 +15,29 @@ class ServiceProvider extends ServiceFactory
     /** @var Model */
     protected $modelInstance;
 
+    /** @var Model */
+    protected $model;
+
+    protected $page = 1;
+
+    protected $limit = 20;
+
     public function __construct()
     {
         if (!empty($this->model) && class_exists($this->model)){
             $model = $this->model;
             $this->modelInstance = new $model;
         }
+        $this->setPageParams();
     }
 
-    public function find($data){
-        return $this->modelInstance->find($data);
+    public function get($data){
+        return $this->model::find($data);
+    }
+
+    public function setPageParams(){
+        /** @var Request $request */
+        $this->page = \request()->param('page',1);
+        $this->limit = \request()->param('limit',1);
     }
 }
